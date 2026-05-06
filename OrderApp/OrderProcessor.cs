f16cff866941cedcf283b42a860f61b590c7841e
Saumya Singh <saumyasingh0624@gmail.com>
2026-05-06 13:36:47 +0530

    added Order model and OrderProcessor with delegate and event

---
+++ OrderApp/OrderProcessor.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace OrderApp
+{
+    public delegate void OrderPlacedHandler(Order order);
+
+    public class OrderProcessor
+    {
+        public event OrderPlacedHandler OnOrderPlaced;
+
+        public void ProcessOrder(Order order)
+        {
+            Console.WriteLine("Order Placed: " + order.OrderId);
+            
+            // trigger the event if anyone is subscribed
+            if (OnOrderPlaced != null)
+            {
+                OnOrderPlaced(order);
+            }
+        }
+    }
+}
